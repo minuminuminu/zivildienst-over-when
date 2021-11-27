949855b21f552bed7fb4f477263729d10feb177f
Minu Kim <minu.kim2003@gmail.com>
2021-11-27 19:43:37 +0100

    add months and days calc

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -25,17 +25,28 @@ export const Test = () => {
     setTime(new Date());
   }
 
-  let calcDate = (date) => {
-    let month = time.toLocaleDateString().split("/");
-    let specifiedMonth = date.split("/");
-    let result =
-      getMonthObj(month[0], month[2]).days - parseInt(specifiedMonth[0]);
+  const calcDate = (date) => {
+    let now = time.toLocaleDateString().split("/");
+    let specifiedDate = date.split("/");
 
-    // console.log(getMonthObj(month[0], month[2]).name);
-    // console.log(result);
+    let nowMonth = {
+      no: getMonthObj(now[1], now[2]).no,
+      days: getMonthObj(now[1], now[2]).days,
+    };
+    let specifiedMonth = {
+      no: getMonthObj(date[1], date[2]).no,
+      days: getMonthObj(date[1], date[2]).days,
+    };
+
+    let remainingDays =
+      nowMonth.days - parseInt(now[0]) + parseInt(specifiedDate[0]);
+    let remainingMonths = parseInt(specifiedDate[1]) + (12 - nowMonth.no);
+    // console.log(
+    //   `${remainingMonths - 1} months and ${remainingDays} days left!`
+    // );
   };
 
-  calcDate("24/04/2003");
+  calcDate("24/04/2021");
 
   return <CenterBody>{time.toLocaleString()}</CenterBody>;
 };
